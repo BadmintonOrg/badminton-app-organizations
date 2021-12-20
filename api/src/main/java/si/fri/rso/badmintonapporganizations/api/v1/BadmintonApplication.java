70379b96d333e835f4d99dcb4535c5b46df03570
janezbozic <janez.bozic1@gmail.com>
2021-12-20 20:48:49 +0100

    Added OpenApi

--- api/src/main/java/si/fri/rso/badmintonapporganizations/api/v1/BadmintonApplication.java
+++ api/src/main/java/si/fri/rso/badmintonapporganizations/api/v1/BadmintonApplication.java
@@ -1,8 +1,18 @@
 package si.fri.rso.badmintonapporganizations.api.v1;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@OpenAPIDefinition(info = @Info(title = "Badminton app organizations API", version = "v1",
+        contact = @Contact(email = "rso@fri.uni-lj.si"),
+        license = @License(name = "dev"), description = "API for Badminton app organizations."),
+        servers = @Server(url = "http://20.62.136.0:8080/")) //TO-DO
 @ApplicationPath("/v1")
 public class BadmintonApplication extends Application {
 
